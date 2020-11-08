dbe03625631017a1df6ca51821102ba8b9ffc92b
ShivainBansal <72510569+ShivainBansal@users.noreply.github.com>
2020-11-08 18:59:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,81 +1,70 @@
-var bullet;
-var wall;
-var speed;
-var thickness;
-var weight;
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(1600, 400);
 
-  speed=random(223,321);
-  weight=random(30,52);
-  thickness=random(22,83);
+  speed=random(223,321)
+  weight=random(30,52)
 
-  bullet=createSprite(50,200,30,10);
-  bullet.velocityX=speed;
-  bullet.shapeColor=color("white");
 
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=color("white");
+  thickness=random(22,83)
+
+
+
+
+  
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
 }
+
 
 function draw() {
-  background("black");  
- 
-  if (wall.x-bullet.x < (bullet.width+wall.width)/2){
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-   bullet.velocityX=0;
-   var deformation=0.5 * weight * speed * speed/22509;
-   if (deformation>180)
-   {
-     bullet.shapeColor=color(255,0,0);
-   }
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-   if (deformation<180 && deformation>100)
-   {
-     bullet.shapeColor=color(230,230,0);
-   }
+	
 
-   if (deformation<100)
-   {
-     bullet.shapeColor=color(0,225,0);
-   }
-
-
-}
- 
- if (hasCollided(bullet,wall)){
-
- bullet.velocityX=0;
- var damage=0.5 * weight * speed * speed/(thickness * thicknes * thicknes);
-
- if (damage>10){
-
- wall.shapeColor=color(225,0,0);
-
- }
- if (damage<10){
-
-  wall.shapeColor=color(0,225,0);
- 
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
 
- }
- 
- 
+
   drawSprites();
+ 
 }
 
-function hasCollided(bullet,wall){
 
-bulletRightEdge=bullet.x+bullet.width;
-wallLeftEdge=wall.x;
-
-if(bulletRightEdge>=wallLeftEdge){
-
-  return true
-
-}
-return false;
-
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
